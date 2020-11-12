cc710e9fcdadd4f71c95f4130fec0e4e34da397b
zhoujiapeng <zhoujiapeng@meicai.cn>
2020-11-12 21:58:23 +0800

    asd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const app = express()
 const port = 80
-
+var bodyParser = require('body-parser');
 
 app.all('*', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,10 +14,27 @@ app.all('*', (req, res, next) => {
     next();
 });
 
-app.get('/getToken',async (req, res) => {
+app.use(bodyParser.json())
+
+app.all('/getToken',async (req, res) => {
     let token = await assumeRole()
-    token.region='oss-cn-beijing'
+
     let result={
+        code:0,
+        data:token
+    }
+    result=JSON.stringify(result);
+    res.send(result);
+})
+app.all('/getUrl',async (req, res) => {
+    body = req.body;
+    let access_key_id = body.access_key_id
+    let access_key_secret = body.access_key_secret
+    let region = body.region
+    let file_path = body.file_path
+    let token = await Sign(access_key_id,access_key_secret,region,file_path)
+
+    let result = {
         code:0,
         data:token
     }
@@ -26,7 +43,7 @@ app.get('/getToken',async (req, res) => {
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-//OSS部分
+//OSS
 
 let OSS = require('ali-oss');
 let STS=OSS.STS
@@ -51,7 +68,20 @@ async function assumeRole () {
     try {
         let token = await sts.assumeRole(
             'acs:ram::1885503572704933:role/salary', policy, 15*60, 'upload');
-        return token.credentials
+        return token
+    } catch (e) {
+        console.log(e);
+    }
+}
+async function Sign (accessKeyId,accessKeySecret,region,file_name) {
+    try {
+        let client = new OSS({
+            region: accessKeyId,
+            accessKeyId: accessKeySecret,
+            accessKeySecret: region
+        })
+        const url = client.signatureUrl(file_name);
+        return url;
     } catch (e) {
         console.log(e);
     }
